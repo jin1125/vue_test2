bbfadc7257a4ecee9b70290cf6b4eb5b86ef00e1
jin1125 <shogo.jinta@gmail.com>
2022-03-16 19:14:19 +0900

    108

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -2,7 +2,11 @@
   <v-app>
     <Header/>
     <v-main>
-      <router-view/>
+      <v-container>
+        <router-view
+          @add-book-list="addBook"
+        />
+      </v-container>
     </v-main>
     <Footer/>
   </v-app>
@@ -12,15 +16,49 @@
 import Header from '@/global/Header.vue';
 import Footer from '@/global/Footer.vue';
 
+const STORAGE_KEY = 'books'
+
 export default {
   name: 'App',
   components: {
     Header,
     Footer
   },
-
-  data: () => ({
-    //
-  }),
+  data(){
+    return {
+      books: [],
+      newBook: null
+    }
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch(e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.books.lenght,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: '',
+        memo: ''
+      });
+      this.saveBooks();
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    }
+  }
 };
 </script>

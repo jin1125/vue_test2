b3cc487f8662ef59f85b0afaf2e1fa3129413d6a
jin1125 <shogo.jinta@gmail.com>
2022-03-17 10:23:13 +0900

    122

---
+++ section9/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <button @click="addCount">+10</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    // increment() {
+    //   this.$store.commit('increment')
+    // },
+    // addCount() {
+    //   this.$store.commit('addCount', {
+    //     value: 10
+    //   })
+    // }
+    increment() {
+      this.$store.dispatch('incrementAction')
+    },
+    addCount() {
+      this.$store.dispatch('addCountAction', {
+        value: 10
+      })
+    }
+
+  }
+}
+</script>
---
+++ section9/vuex/src/store/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  getters: {
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    addCount(state, payload){
+      state.count += payload.value
+    }
+  },
+  actions: {
+    incrementAction(context) {
+      context.commit('increment')
+    },
+    addCountAction({commit}, payload) {
+      commit('addCount', payload)
+    }
+  },
+  modules: {
+  }
+})

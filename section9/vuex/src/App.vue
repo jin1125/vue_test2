6a834ee7dd4e5ef24d37af2c3b047323fadaf555
jin1125 <shogo.jinta@gmail.com>
2022-03-17 21:43:21 +0900

    126

--- section9/vuex/src/App.vue
+++ section9/vuex/src/App.vue
@@ -5,6 +5,9 @@
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
+    <button @click="setLogin">
+      login name
+    </button>
     {{ $store.state.count }}
     <br>
     <ul>
@@ -18,7 +21,17 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
+
 export default {
+  methods: {
+    ...mapActions('auth', ['setLoginUser']),
+    setLogin() {
+      this.setLoginUser({
+        name: 'otani'
+      })
+    }
+  },
   computed: {
     visibleUsers() {
       return this.$store.getters.visibleUsers

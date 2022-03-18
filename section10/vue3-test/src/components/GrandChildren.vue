ce4dcf4230c24a7b782752a7175c28437ff81074
jin1125 <shogo.jinta@gmail.com>
2022-03-18 10:28:00 +0900

    133

---
+++ section10/vue3-test/src/components/GrandChildren.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    GrandChildren
+  </div>
+  <div>
+    {{ userName }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  inject: ['userName']
+}
+</script>
+
+<style>
+
+</style>
---
+++ section10/vue3-test/src/views/Children.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Children
+    <Grand-Children />
+  </div>
+</template>
+
+<script>
+import GrandChildren from '@/components/GrandChildren'
+
+export default {
+  components: {
+    GrandChildren
+  }
+}
+</script>
+
+<style>
+
+</style>
